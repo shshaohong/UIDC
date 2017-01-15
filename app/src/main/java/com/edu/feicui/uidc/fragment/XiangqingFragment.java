5e8c0ed53ebdbaf26725f46e450b705b441a26f7
shshaohong <960020167@qq.com>
2017-01-15 13:35:18 +0800

    噢噢

---
+++ app/src/main/java/com/edu/feicui/uidc/fragment/XiangqingFragment.java
@@ -0,0 +1,12 @@
+package com.edu.feicui.uidc.fragment;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Administrator on 2017-1-15.
+ */
+
+public class XiangqingFragment extends DialogFragment {
+
+
+}
